34beff4e91135b39d678b1c50b65db2bbd319330
shengxiaomai <734978582@qq.com>
2020-06-01 17:16:55 +0800

    增加组件

---
+++ Conf/Config.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/5
+# @Author  : sheng
+# @File    : Log.py
+
+"""
+封装log方法
+
+"""
+
+from configparser import ConfigParser
+import os
+
+class Config:
+    # titles:
+    TITLE_DEBUG = "private_debug"
+    TITLE_EMAIL = "mail"
+
+    # values:
+    # [debug\release]
+    VALUE_TESTER = "tester"
+    VALUE_ENVIRONMENT = "environment"
+    VALUE_VERSION_CODE = "versionCode"
+    VALUE_HOST = "host"
+    VALUE_UserHost = "user_host"
+    VALUE_GroupHost="groupim_host"
+    VALUE_LOGIN_HOST = "loginHost"
+    VALUE_LOGIN_INFO = "loginInfo"
+
+    # [mail]
+    VALUE_SMTP_SERVER = "smtpserver"
+    VALUE_SENDER = "sender"
+    VALUE_RECEIVER = "receiver"
+    VALUE_USERNAME = "username"
+    VALUE_PASSWORD = "password"
+
+    # path
+    path_dir = str(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+
+    def __init__(self):
+
+        self.config = ConfigParser()
+        self.conf_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini')
+        self.xml_report_path = Config.path_dir + '/Report/xml'
+        self.html_report_path = Config.path_dir + '/Report/html'
+
+
+        if not os.path.exists(self.conf_path):
+            raise FileNotFoundError("请确保配置文件存在！")
+        self.config.read(self.conf_path, encoding='utf-8')
+
+        self.tester_debug = self.get_conf(Config.TITLE_DEBUG, Config.VALUE_TESTER)
+        self.environment_debug = self.get_conf(Config.TITLE_DEBUG, Config.VALUE_ENVIRONMENT)
+        self.versionCode_debug = self.get_conf(Config.TITLE_DEBUG, Config.VALUE_VERSION_CODE)
+        self.host_debug = self.get_conf(Config.TITLE_DEBUG, Config.VALUE_HOST)
+        self.user_host_debug = self.get_conf(Config.TITLE_DEBUG, Config.VALUE_UserHost)
+        self.groupim_debug = self.get_conf(Config.TITLE_DEBUG, Config.VALUE_GroupHost)
+        self.loginHost_debug = self.get_conf(Config.TITLE_DEBUG, Config.VALUE_LOGIN_HOST)
+        self.loginInfo_debug = self.get_conf(Config.TITLE_DEBUG, Config.VALUE_LOGIN_INFO)
+
+        self.smtpserver = self.get_conf(Config.TITLE_EMAIL, Config.VALUE_SMTP_SERVER)
+        self.sender = self.get_conf(Config.TITLE_EMAIL, Config.VALUE_SENDER)
+        self.receiver = self.get_conf(Config.TITLE_EMAIL, Config.VALUE_RECEIVER)
+        self.username = self.get_conf(Config.TITLE_EMAIL, Config.VALUE_USERNAME)
+        self.password = self.get_conf(Config.TITLE_EMAIL, Config.VALUE_PASSWORD)
+
+    def get_conf(self, title, value):
+        """
+        配置文件读取
+        :param title:
+        :param value:
+        :return:
+        """
+        return self.config.get(title, value)
